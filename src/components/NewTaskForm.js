12d0289dee1bc0f7d8c44ab8a8aaf7515e2824b4
Joseph <JosephSped23@gmail.com>
2024-04-30 15:38:53 -0400

    passing lab tests

--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, {useState} from "react";
 
-function NewTaskForm() {
+function NewTaskForm({onTaskFormSubmit}) {
+const [detailValue, setDetailValue]= useState("")
+const [categoryValue, setCategoryValue]= useState("")
+
+function handleSubmit(e){
+  e.preventDefault()
+  const newTask= {
+    text: detailValue,
+    category: categoryValue
+  }
+onTaskFormSubmit(newTask)
+}
+
+  function handleDetailInput(e){
+    setDetailValue(e.target.value)
+  }
+  function handleChangeCategory(e){
+    setCategoryValue(e.target.value)
+  }
+  console.log(categoryValue)
   return (
-    <form className="new-task-form">
+    <form  onSubmit ={handleSubmit} className="new-task-form">
       <label>
         Details
-        <input type="text" name="text" />
+        <input onChange={handleDetailInput} value={detailValue} type="text" name="text" />
       </label>
       <label>
         Category
-        <select name="category">
-          {/* render <option> elements for each category here */}
+        <select onChange={handleChangeCategory} value={categoryValue}  name="category">
+          <option type="text" name="text">All</option>
+          <option type="text" name="text">Code</option>
+          <option type="text" name="text">Food</option>
+          <option type="text" name="text">Misc</option>
+          <option type="text" name="text">Money</option>
         </select>
       </label>
       <input type="submit" value="Add task" />
